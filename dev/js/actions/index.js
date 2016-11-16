e5394a5c269a178aa2862643af1a12c917817b22
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-16 14:51:42 +0100

    getting all articles redux action

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -1,7 +1,13 @@
-export const selectUser = (user) => {
-    console.log("You clicked on user: ", user.first);
-    return {
-        type: 'USER_SELECTED',
-        payload: user
-    }
-};
+import {GET_ARTICLES} from './types';
+import axios from 'axios';
+
+const API_URL = "http://localhost:5000/api/v1";
+
+export function getArticles(){
+  const request = axios.get('${API_URL}/articles');
+
+  return{
+    type: GET_ARTICLES,
+    playload: request
+  };
+}
