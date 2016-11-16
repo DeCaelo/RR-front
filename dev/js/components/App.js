ac24ee6520076ecfb9ab18d414808898e2259c04
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-16 11:55:13 +0100

    Routes with React Router

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import { render } from 'react-dom';
 require('../../scss/style.scss');
 
 class App extends Component{
   render(){
     return (
        <div>
-
+        Navbar
+        {this.props.children}
       </div>
     );
   }
