5e51c9b301d08d7da0d6f4a2d86bcd5b6dd19283
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-17 10:58:55 +0100

    redux form

--- dev/js/components/NewArticle.js
+++ dev/js/components/NewArticle.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import {createArticle} from '../actions/index';
 
-export default class NewArticle extends Component{
+class NewArticle extends Component{
   render(){
+    const {fields:{title}, handleSubmit} = this.props;
+
     return(
       <div className="container">
-      New Article Create Page
+      <h1>New Article Create Page</h1>
 
-      form here
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+        <button type="submit" className="btn btn-success">Create</button>
+      </form>
+
       </div>
     );
   }
 }
+
+export default reduxForm({
+  form: 'NewArticleForm',
+  fields: ['title']
+}, null, {createArticle})(NewArticle);
--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import ArticlesReducer from './articles_reducer'
+import {reducer as formReducer} from 'redux-form';
 
 const allReducers = combineReducers({
-  articles: ArticlesReducer
+  articles: ArticlesReducer,
+  form: formReducer
 });
 
 export default allReducers
